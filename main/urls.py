c5f70d67cf14398bae40b5b4a7e9d8d1799b8a76
Theodore Keloglou <zf@sirodoht.com>
2024-06-04 22:22:53 +0100

    add stripe payments functionality

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,12 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.index, name="index"),
+    # contacts
     path("add-contact/", views.ContactCreate.as_view(), name="contact_create"),
     path("contacts/", views.ContactList.as_view(), name="contact_list"),
+    # domains
     path("add-domain/", views.DomainCreate.as_view(), name="domain_create"),
+    # payments
+    path("checkout/success/", views.checkout_success, name="checkout_success"),
+    path("checkout/failure/", views.checkout_failure, name="checkout_failure"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+import stripe
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.conf import settings
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView
@@ -14,7 +18,7 @@ def index(request):
         request,
         "main/index.html",
         {
-            "domain_list": models.Domain.objects.all(),
+            "domain_list": models.Domain.objects.filter(pending=False),
         },
     )
 
@@ -55,9 +59,59 @@ class DomainCreate(LoginRequiredMixin, CreateView):
         "nameserver3",
     ]
     template_name = "main/domain_create.html"
-    success_url = reverse_lazy("index")
+    success_url = reverse_lazy("checkout")
 
     def form_valid(self, form):
+        # save domain instance
         self.object = form.save(commit=False)
-        centralnic.register_domain(self.object)
-        return super().form_valid(form)
+        self.object.pending = True
+        self.object.save()
+
+        # create checkout instance
+        models.Checkout.objects.create(domain=self.object)
+
+        # start checkout
+        stripe.api_key = settings.STRIPE_API_KEY
+        checkout_session = stripe.checkout.Session.create(
+            customer_email=self.request.user.email,
+            line_items=[
+                {
+                    "price": "price_1PO27uKcGejlgwEvMViklyCs",
+                    "quantity": 1,
+                },
+            ],
+            mode="payment",
+            success_url=f"{settings.CANONICAL_URL}{reverse_lazy('checkout_success')}",
+            cancel_url=f"{settings.CANONICAL_URL}{reverse_lazy('checkout_failure')}",
+        )
+        response = HttpResponseRedirect(checkout_session.url)
+        response.status_code = 303
+        return response
+
+
+def checkout_success(request):
+    domain = models.Domain.objects.get(pending=True)
+    checkout = models.Checkout.objects.get(domain=domain)
+
+    # register domain
+    centralnic.register_domain(domain)
+
+    # complete registration on success
+    checkout.delete()
+    domain.pending = False
+    domain.save()
+
+    # respond with message
+    messages.success(request, "payment complete")
+    return redirect("index")
+
+
+def checkout_failure(request):
+    # cleanup pending domain and checkout instances
+    domain = models.Domain.objects.filter(pending=True)
+    domain.delete()
+    models.Checkout.objects.all().delete()
+
+    # respond with message
+    messages.error(request, "payment failed")
+    return redirect("domain_create")

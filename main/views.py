fb7566973d5a5d9b016faff6962e64195f246d4b
Theodore Keloglou <zf@sirodoht.com>
2024-06-10 23:29:32 +0100

    change domains and contacts to belong to single users

--- main/views.py
+++ main/views.py
@@ -1,5 +1,4 @@
 import stripe
-from django.contrib.auth.views import LogoutView as DjLogoutView
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.http import HttpResponseRedirect
@@ -9,116 +8,15 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.decorators import login_required
 
 from main import models, centralnic, forms
 
 
+# Landing - public
 def index(request):
     if request.user.is_authenticated:
         return redirect("domain_list")
     return render(request, "main/index.html")
-
-
-class DomainList(LoginRequiredMixin, ListView):
-    model = models.Domain
-
-    def get_queryset(self):
-        return models.Domain.objects.filter(pending=False)
-
-
-class ContactCreate(LoginRequiredMixin, CreateView):
-    model = models.Contact
-    fields = [
-        "first_name",
-        "last_name",
-        "street",
-        "city",
-        "postal",
-        "country",
-        "phone",
-        "email",
-    ]
-    template_name = "main/contact_create.html"
-    success_url = reverse_lazy("contact_list")
-
-    def form_valid(self, form):
-        self.object = form.save()
-        centralnic.create_contact(self.object)
-        return super().form_valid(form)
-
-
-class ContactList(LoginRequiredMixin, ListView):
-    model = models.Contact
-
-
-class DomainCreate(LoginRequiredMixin, CreateView):
-    model = models.Domain
-    fields = [
-        "domain_name",
-        "contact",
-        "nameserver0",
-        "nameserver1",
-        "nameserver2",
-        "nameserver3",
-    ]
-    template_name = "main/domain_create.html"
-    success_url = reverse_lazy("checkout")
-
-    def form_valid(self, form):
-        # save domain instance
-        self.object = form.save(commit=False)
-        self.object.pending = True
-        self.object.save()
-
-        # create checkout instance
-        models.Checkout.objects.create(domain=self.object)
-
-        # start checkout
-        stripe.api_key = settings.STRIPE_API_KEY
-        checkout_session = stripe.checkout.Session.create(
-            customer_email=self.request.user.email,
-            line_items=[
-                {
-                    "price": "price_1PO27uKcGejlgwEvMViklyCs",
-                    "quantity": 1,
-                },
-            ],
-            mode="payment",
-            success_url=f"{settings.CANONICAL_URL}{reverse_lazy('checkout_success')}",
-            cancel_url=f"{settings.CANONICAL_URL}{reverse_lazy('checkout_failure')}",
-        )
-        response = HttpResponseRedirect(checkout_session.url)
-        response.status_code = 303
-        return response
-
-
-def checkout_success(request):
-    domain = models.Domain.objects.get(pending=True)
-    checkout = models.Checkout.objects.get(domain=domain)
-
-    # register domain
-    centralnic.register_domain(domain)
-
-    # complete registration on success
-    checkout.delete()
-    domain.pending = False
-    domain.save()
-
-    # respond with message
-    messages.success(request, "payment complete")
-    return redirect("index")
-
-
-def checkout_failure(request):
-    # cleanup pending domain and checkout instances
-    domain = models.Domain.objects.filter(pending=True)
-    domain.delete()
-    models.Checkout.objects.all().delete()
-
-    # respond with message
-    messages.error(request, "payment failed")
-    return redirect("domain_create")
 
 
 # User accounts
@@ -139,3 +37,112 @@ class UserCreate(CreateView):
         login(self.request, user)
         messages.success(self.request, self.success_message)
         return HttpResponseRedirect(self.get_success_url())
+
+
+# Core: domains and contacts
+class DomainList(LoginRequiredMixin, ListView):
+    model = models.Domain
+
+    def get_queryset(self):
+        return models.Domain.objects.filter(owner=self.request.user, pending=False)
+
+
+class ContactCreate(LoginRequiredMixin, CreateView):
+    model = models.Contact
+    fields = [
+        "first_name",
+        "last_name",
+        "street",
+        "city",
+        "postal",
+        "country",
+        "phone",
+        "email",
+    ]
+    template_name = "main/contact_create.html"
+    success_url = reverse_lazy("contact_list")
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.owner = self.request.user
+        self.object.save()
+        centralnic.create_contact(self.object)
+        return super().form_valid(form)
+
+
+class ContactList(LoginRequiredMixin, ListView):
+    model = models.Contact
+
+    def get_queryset(self):
+        return models.Contact.objects.filter(owner=self.request.user)
+
+
+class DomainCreate(LoginRequiredMixin, CreateView):
+    model = models.Domain
+    fields = [
+        "domain_name",
+        "contact",  # TODO: figure out how not to leak all contacts
+        "nameserver0",
+        "nameserver1",
+        "nameserver2",
+        "nameserver3",
+    ]
+    template_name = "main/domain_create.html"
+    success_url = reverse_lazy("checkout")
+
+    def form_valid(self, form):
+        # save domain instance
+        self.object = form.save(commit=False)
+        self.object.owner = self.request.user
+        self.object.pending = True
+        self.object.save()
+
+        # create checkout instance
+        models.Checkout.objects.create(domain=self.object)
+
+        # start checkout
+        stripe.api_key = settings.STRIPE_API_KEY
+        checkout_session = stripe.checkout.Session.create(
+            customer_email=self.request.user.email,
+            line_items=[
+                {
+                    "price": settings.STRIPE_PRICE,
+                    "quantity": 1,
+                },
+            ],
+            mode="payment",
+            success_url=f"{settings.CANONICAL_URL}{reverse_lazy('checkout_success')}",
+            cancel_url=f"{settings.CANONICAL_URL}{reverse_lazy('checkout_failure')}",
+        )
+        response = HttpResponseRedirect(checkout_session.url)
+        response.status_code = 303
+        return response
+
+
+# Payments - Stripe
+def checkout_success(request):
+    domain = models.Domain.objects.get(pending=True, owner=request.user)
+    checkout = models.Checkout.objects.get(domain=domain)
+
+    # register domain
+    centralnic.register_domain(domain)
+
+    # complete registration on success
+    checkout.delete()
+    domain.pending = False
+    domain.save()
+
+    # respond with message
+    messages.success(request, "registration complete")
+    return redirect("index")
+
+
+def checkout_failure(request):
+    # cleanup pending domain and checkout instances
+    domain = models.Domain.objects.filter(pending=True)
+    domain.delete()
+    models.Checkout.objects.all().delete()
+
+    # respond with message
+    messages.error(request, "payment failed")
+    return redirect("domain_create")
